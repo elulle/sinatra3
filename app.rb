a381ba4de2d9dede75e2e306a026f4329806d184
Elina Desaine <edesaine@ticklo.com>
2013-08-12 15:28:58 +0100

    individual simpsons

--- app.rb
+++ app.rb
@@ -26,6 +26,19 @@ get '/simpsons' do
   erb :simpsons_view
 end
 
+get '/simpsons/:name' do
+  @s = list_of_simpsons.select {|s| s[:name].downcase == params[:name]}.first
+  if @s
+    # we have a simpson
+    erb :indi_simp
+  else
+    # they gave a bad name
+    erb :missing_simpson
+  end
+
+end
+
+
 def list_of_countries
   [ 
     ["UK", "London"],
@@ -38,8 +51,8 @@ def list_of_countries
 end
 
 def list_of_simpsons
-  [
-    { :name => "Homer", :age => 38, :gender => "M", :catchphrase => "D'oh!", :pic => "http://simpsons.wikia.com/wiki/Homer_Simpson?file=Homer_Simpson.png"},
+   [
+    { :name => "Homer", :age => 38, :gender => "M", :catchphrase => "D'oh!", :pic => "http://images.wikia.com/simpsons/images/b/bd/Homer_Simpson.png"},
     { :name => "Marge", :age => 34, :gender =>"F" , :catchphrase => "Hmm...", :pic =>"http://images.wikia.com/simpsons/images/0/0b/Marge_Simpson.png"},
     { :name => "Bart", :age => 10, :gender => "M", :catchphrase =>  "Eat My Shorts!", :pic =>"http://images.wikia.com/simpsons/images/6/65/Bart_Simpson.png"},
     { :name => "Lisa", :age =>8, :gender =>"F", :catchphrase => "Trust in yourself and you can achieve anything.", :pic =>"http://images.wikia.com/simpsons/images/5/57/Lisa_Simpson2.png"},
